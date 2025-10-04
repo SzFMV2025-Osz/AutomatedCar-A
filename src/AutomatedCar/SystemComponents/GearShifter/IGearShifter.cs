82f76230e240efc55e903e013bb6ef1098547a08
BAY42M <163119037+BAY42M@users.noreply.github.com>
2025-10-04 17:16:43 +0200

    gearbox, váltó, sebességek váltása, torque és kmh számítás

---
+++ src/AutomatedCar/SystemComponents/GearShifter/IGearShifter.cs
@@ -0,0 +1,30 @@
+﻿namespace AutomatedCar.SystemComponents.GearShifter
+{
+    using System;
+    using Avalonia.Input;
+
+    public enum Gear
+    {
+        P = 0,
+        R = 1,
+        N = 2,
+        D = 3,
+    }
+
+    public interface IGearShifter
+    {
+        public Gear CurrentGear { get; set; }
+
+        public int CurrentGearNumber { get; }
+
+        public double SpeedKph { get; } // convenience (km/h)
+
+        public double RPM { get; } // engine RPM
+
+        public void Shift(Key key); // keep your existing input method
+
+        public void ShiftUp();
+
+        public void ShiftDown();
+    }
+}
--- src/AutomatedCar/SystemComponents/VirtualFunctionBus.cs
+++ src/AutomatedCar/SystemComponents/VirtualFunctionBus.cs
@@ -1,13 +1,16 @@
 namespace AutomatedCar.SystemComponents
 {
-    using AutomatedCar.SystemComponents.Packets;
     using System.Collections.Generic;
+    using AutomatedCar.SystemComponents.GearShifter;
+    using AutomatedCar.SystemComponents.Packets;
 
     public class VirtualFunctionBus : GameBase
     {
         private List<SystemComponent> components = new List<SystemComponent>();
 
         public IReadOnlyDummyPacket DummyPacket { get; set; }
+
+        public IGearShifter GearShifter { get; set; }
 
         public void RegisterComponent(SystemComponent component)
         {
