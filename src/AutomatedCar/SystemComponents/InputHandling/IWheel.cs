910c21c85d279bf411e47251b22020773a2a3294
MPeti <monos.peter@stud.uni-obuda.hu>
2025-10-14 11:54:12 +0200

    Engine, InputHandling interfacek

---
+++ src/AutomatedCar/SystemComponents/InputHandling/IWheel.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.SystemComponents.InputHandling
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IWheel
+    {
+        public double AngleAsDeagree {  get; set; }
+    }
+}
