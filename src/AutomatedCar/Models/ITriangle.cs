8da42305bbb39707701b7452a9c9796140fa89ae
CsiszarSoma <csiszardesoma@gmail.com>
2025-10-20 13:07:05 +0200

    Issue megoldava

---
+++ src/AutomatedCar/Models/ITriangle.cs
@@ -0,0 +1,22 @@
+﻿namespace AutomatedCar.Models
+
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ITriangle
+    {
+
+        double Angle { get; }
+        double Distance { get; }
+
+        List<WorldObject> GetIntersections();
+
+        void UpdateRelativePosition();
+
+        void Tick();
+    }
+}
