92d329f2cb19fe8128895f3d6b865a7177179d7d
Balázs Zick <balazs.zick2@gmail.com>
2025-10-27 17:58:29 +0100

    Collisions implemented
    resolves #13

---
+++ src/AutomatedCar/SystemComponents/CollisionDetectionService.cs
@@ -0,0 +1,55 @@
+namespace AutomatedCar.SystemComponents;
+
+using Helpers;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+///     This class is responsible for handling collisions with <c>Collidable</c> WorldObjects.
+/// </summary>
+public class CollisionDetectionService : SystemComponent
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="CollisionDetectionService" /> class.
+    /// </summary>
+    /// <param name="virtualFunctionBus">The <c>VirtualFunctionBus</c> needs to be given, in order to register it.</param>
+    public CollisionDetectionService(VirtualFunctionBus virtualFunctionBus)
+        : base(virtualFunctionBus)
+    {
+        this.virtualFunctionBus.RegisterComponent(this);
+    }
+
+    /// <summary>
+    ///     This event will be invoked, if the car has collided with a <c>Collidable</c> WorldObject.
+    /// </summary>
+    public event EventHandler<WorldObject> OnCollided;
+
+    /// <summary>
+    ///     Every Tick we inspect if the nearby collideable objects are indeed in the car's polygonal shape.
+    /// </summary>
+    public override void Process()
+    {
+        AutomatedCar currentCar = World.Instance.ControlledCar;
+
+        // Get all collidable WorldObjects in nearby vicinity of the currently controlled car.
+        List<WorldObject> nearbyObjects = World.Instance.WorldObjects
+            .Where(x => GeometryHelper.DistanceBetweenObjects(x, currentCar) < 435 && x.Collideable)
+            .ToList();
+
+        nearbyObjects.ForEach(this.InvokeEventIfCollided);
+    }
+
+    /// <summary>
+    ///     Checks if any of a WorldObjects's Points are in the car's absolute positioned polygonal shape.
+    /// </summary>
+    /// <param name="x">The WorldObject we want to check, can be assumed that it is a Collideable.</param>
+    private void InvokeEventIfCollided(WorldObject x)
+    {
+        if (GeometryHelper.CheckWorldObjectInCar(x))
+        {
+            this.OnCollided?.Invoke(this, x);
+        }
+    }
+}
