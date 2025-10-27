8e45d340552cdc02058dac20454f01de0ef85a21
Zolesz <Zolesz@ZOLESZ-PC>
2025-10-27 21:15:02 +0100

    Implement basic path following for pedestrians and NPC cars
    # Conflicts:
    #	src/AutomatedCar/Models/World.cs

---
+++ src/AutomatedCar/Models/NPC/Pedestrian.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.Models.NPC
+{
+    using System.Collections.Generic;
+
+    public class Pedestrian : PathFollowingNpcBase
+    {
+        public Pedestrian(int x, int y, string sprite = "pedestrian")
+            : base(x, y, sprite) { }
+
+        // Gyalogos fordulhat gyorsabban
+        protected override double AngularSpeedDegPerSec => 300.0;
+
+        public void Load(int speed, bool repeating, int currentPoint, List<NPCPath> points)
+        {
+            this.Speed = speed;
+            this.ConfigurePath(points, repeating, currentPoint);
+        }
+    }
+}
