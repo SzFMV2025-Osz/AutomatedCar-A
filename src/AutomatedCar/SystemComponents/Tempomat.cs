f03352cfc5840a2db978eaf39ae77b465c0453ea
nyikos.dani09 <nyikos.dani09@gmail.com>
2025-11-25 19:36:28 +0100

    Tempomat class inherited, all property declarated

--- src/AutomatedCar/SystemComponents/Tempomat.cs
+++ src/AutomatedCar/SystemComponents/Tempomat.cs
@@ -1,12 +1,36 @@
 ﻿namespace AutomatedCar.SystemComponents
 {
+    using AutomatedCar.SystemComponents.Packets.Input_Packets;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
-    internal class Tempomat
+    public class Tempomat : SystemComponent
     {
+        VirtualFunctionBus virtualFunctionBus;
+        public int UserSetSpeed { get; set; }
+        public int LimitSpeed { get; set; }
+        private TempomatPacket tempomatPacket;
+        public int CurrentSpeed { get; set; }
+        public bool IsEnabled { get; set; }
+        public int BrakePercentage { get; set; }
+        public int ThrottlePercentage { get; set; }
+        private const int minimumSpeed = 30;
+        private const int maximumSpeed = 160;
+        private const int speedChangeInterval = 10;
+        int GoalSpeed
+        {
+            get  ; set;
+        }
+        public Tempomat(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
