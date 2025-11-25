009619229d55edbe768d490a5e03be180d4251e3
nyikos.dani09 <nyikos.dani09@gmail.com>
2025-11-25 19:29:46 +0100

    IReadOnlyTempomatPacket.cs, TempomatPacket.cs added

---
+++ src/AutomatedCar/SystemComponents/Packets/Input_Packets/IReadOnlyTempomatPacket.cs
@@ -0,0 +1,18 @@
+﻿namespace AutomatedCar.SystemComponents.Packets.Input_Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlyTempomatPacket
+    {
+        int userSetSpeed { get; set; }
+        int limitSpeed { get; set; }
+        int currentSpeed { get; set; }
+        bool isEnabled { get; set; }
+        int BrakePercentage { get; set; }
+        int ThrottlePercentage { get; set; }
+    }
+}
