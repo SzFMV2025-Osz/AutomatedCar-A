526ccf46ce0ba6a13fcb17d9b64d06064aecda4f
nyikos.dani09 <nyikos.dani09@gmail.com>
2025-12-02 13:24:42 +0100

    IReadOnlyAEBInputPacket inteface added and implemented in AEBIP.cs class

---
+++ src/AutomatedCar/SystemComponents/Packets/Input_Packets/IReadOnlyAEBInputPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace AutomatedCar.SystemComponents.Packets.Input_Packets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IReadOnlyAEBInputPacket : IReadOnlyInputDevicePacket
+    {
+        bool WarningOver70kmph { get; set; }
+
+        bool WarningAvoidableCollision { get; set; }
+    }
+}
